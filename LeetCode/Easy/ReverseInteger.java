4c9d32e1ab5439612e34a9b758319398ddfd1f90
alui <alex.lui858@gmail.com>
2019-05-05 13:26:54 -0700

    finished ReverseInteger

--- LeetCode/Easy/ReverseInteger.java
+++ LeetCode/Easy/ReverseInteger.java
@@ -1,8 +1,27 @@
 public class ReverseInteger{
-	public int reverse(int x) {
-        
+	public static int reverse(int x) {
+		char[] charArr;
+		if (x > -1) {
+        	charArr = Integer.toString(x).toCharArray();
+        } else {
+        	String stringx = Integer.toString(x);
+        	stringx = stringx.substring(1, stringx.length());
+        	charArr = stringx.toCharArray();
+        }
+        String result = "";
+        for (int i = charArr.length - 1; i > -1; i--) {
+        	result += charArr[i];
+        }
+ 
+        try{
+        	if (x < 0) return Integer.parseInt("-" + result);
+        	return Integer.parseInt(result);
+        } catch (Exception e){
+        }
+        return 0;
+
     }
     public static void main(String[] args){
-    	
+    	System.out.println(Integer.toString(reverse(-123)));
     }
 }
