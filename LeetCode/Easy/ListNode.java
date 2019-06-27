da7761a3b8b9b198cafb66ff39e37443795a5f8a
alui <alex.lui858@gmail.com>
2019-06-26 21:59:47 -0700

    finished MergeLists

--- LeetCode/Easy/ListNode.java
+++ LeetCode/Easy/ListNode.java
@@ -20,33 +20,5 @@ public class ListNode{
 		one.next = two;
 		two.next = three;
 		System.out.println(one.printNode());
-	}
-	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) return null;
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-        ListNode mergedList = new ListNode(-1);
-        mergedList.merge(l1, l2);
-        return mergedList;
-    }
-	public void merge(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) return;
-        if (l1 == null) {
-        	this.next = l2; 
-        	return;
-        }
-        if (l2 == null) {
-        	this.next = l1; 
-        	return;
-        }
-        if (l1.val > l2.val) {
-        	ListNode mergee = new ListNode(l2.val);
-        	this.next = mergee;
-        	mergee.merge(l1, l2.next);
-        } else {
-        	ListNode mergee = new ListNode(l1.val);
-        	this.next = mergee;
-        	mergee.merge(l1.next, l2);
-        }
-    }
+	}    
 }
--- LeetCode/Easy/MergeLists.java
+++ LeetCode/Easy/MergeLists.java
@@ -22,4 +22,41 @@ public class MergeLists extends ListNode{
 	MergeLists(){
 		super(1);
 	}
+	
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode mergedList;
+        if (l1.val > l2.val) {
+            mergedList = new ListNode(l2.val);
+            merge(l1, l2.next, mergedList);
+        } else {
+            mergedList = new ListNode(l1.val);
+            merge(l1.next, l2, mergedList);
+        }
+
+        return mergedList;
+    }
+	public static void merge(ListNode l1, ListNode l2, ListNode parent) {
+        if (l1 == null && l2 == null) return;
+        if (l1 == null) {
+        	parent.next = l2; 
+        	return;
+        }
+        if (l2 == null) {
+        	parent.next = l1; 
+        	return;
+        }
+        if (l1.val > l2.val) {
+        	ListNode mergee = new ListNode(l2.val);
+        	parent.next = mergee;
+        	merge(l1, l2.next, mergee);
+        } else {
+        	ListNode mergee = new ListNode(l1.val);
+        	parent.next = mergee;
+        	merge(l1.next, l2, mergee);
+        }
+    }
 }
