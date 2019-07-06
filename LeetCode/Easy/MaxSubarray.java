1f294048a6973521e1a3980273342992c86f37b6
alui <alex.lui858@gmail.com>
2019-07-06 05:52:31 -0700

    decreased space (1ms/90,24%,37.3MB/99.3%)

--- LeetCode/Easy/MaxSubarray.java
+++ LeetCode/Easy/MaxSubarray.java
@@ -1,27 +1,24 @@
 public class MaxSubarray {
 	public int maxSubArray(int[] nums) {
-    	int[] sums = new int[nums.length];
-
+		int sums = 0;
     	int max = nums[0];
-    	int maxForwardSum = nums[0];
-    	int maxForwardIndex = 0;
+    	int maxSum = nums[0];
 
-    	if (nums[0] < 0) sums[0] = 0;
-    	else sums[0] = nums[0];
+    	if (nums[0] < 0) sums = 0;
+    	else sums = nums[0];
 
     	for (int i = 1; i < nums.length; i++) {
-    		if (sums[i-1] + nums[i] < 0) {
-    			sums[i] = 0;
+    		if (sums + nums[i] < 0) {
+    			sums = 0;
     		} else {
-    			sums[i] = sums[i-1] + nums[i];
+    			sums += nums[i];
     		}
-    		if (sums[i] > maxForwardSum) {
-    			maxForwardSum = sums[i];
-    			maxForwardIndex = i;
+    		if (sums > maxSum) {
+    			maxSum = sums;
     		}
     		if (nums[i] > max) max = nums[i];
     	}
 
-    	return (maxForwardSum == 0) ? max : maxForwardSum;
+    	return (maxSum == 0) ? max : maxSum;
 	}
 }
