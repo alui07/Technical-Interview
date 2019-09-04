73d9873f97f12c1dc6910fd5e8adc7de1b95749b
alui07 <alex.lui858@gmail.com>
2019-09-04 16:34:46 -0700

    finished #21 (0ms/100%,41MB/8.08%)

---
+++ LeetCode/Java/Easy/MergeLists.java
@@ -0,0 +1,70 @@
+/* 21. Merge Two Sorted Lists
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+import java.util.*;
+import java.lang.*;
+
+public class MergeLists extends ListNode{
+	public static void main (String[] args) {
+		ListNode one1 = new ListNode(1);
+		ListNode two = new ListNode(2);
+		ListNode four1 = new ListNode(4);
+		one1.next = two; two.next = four1;
+
+
+		ListNode one2 = new ListNode(1);
+		ListNode three = new ListNode(3);
+		ListNode four2 = new ListNode(4);
+		one2.next = three; three.next = four2;
+
+		one1.print();
+		one2.print();
+		ListNode mergedList = mergeTwoLists(one1, one2);
+		mergedList.print();
+	}
+	MergeLists(){
+		super(1);
+	}
+	
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode mergedList;
+        if (l1.val > l2.val) {
+            mergedList = new ListNode(l2.val);
+            merge(l1, l2.next, mergedList);
+        } else {
+            mergedList = new ListNode(l1.val);
+            merge(l1.next, l2, mergedList);
+        }
+
+        return mergedList;
+    }
+	public static void merge(ListNode l1, ListNode l2, ListNode parent) {
+        if (l1 == null && l2 == null) return;
+        if (l1 == null) {
+        	parent.next = l2; 
+        	return;
+        }
+        if (l2 == null) {
+        	parent.next = l1; 
+        	return;
+        }
+        if (l1.val > l2.val) {
+        	ListNode mergee = new ListNode(l2.val);
+        	parent.next = mergee;
+        	merge(l1, l2.next, mergee);
+        } else {
+        	ListNode mergee = new ListNode(l1.val);
+        	parent.next = mergee;
+        	merge(l1.next, l2, mergee);
+        }
+    }
+}
