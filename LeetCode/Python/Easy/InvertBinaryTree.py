f081776670160ecd119e6a8f006537617ceaf830
alui <alex.lui858@gmail.com>
2020-07-06 21:19:26 -0700

    finished #226 (36ms/16.02%,13.8MB/68.73%)

---
+++ LeetCode/Python/Easy/InvertBinaryTree.py
@@ -0,0 +1,37 @@
+# 226. Invert Binary Tree
+"""
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None: return
+        Solution.invertTree(self, root.left)
+        Solution.invertTree(self, root.right)
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        return root
